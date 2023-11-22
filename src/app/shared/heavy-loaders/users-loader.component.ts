856533b206326fc6051bdd543bf818e023b1b1fb
GitRockie <150247498+honomonodev@users.noreply.github.com>
2023-11-22 21:48:50 +0100

    Components arquitecture start
    service and interface are pending

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: 
+  `
+  <h1>Hola Mundo</h1>
+  `,
+
+})
+export class UsersLoaderComponent {
+
+}
